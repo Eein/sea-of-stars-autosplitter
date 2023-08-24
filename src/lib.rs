b4423edccd3710ef1c6c5752a5c592fafc5679f2
William Volin <me@williamvolin.com>
2023-08-23 17:35:23 -0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,136 +1,239 @@
+#![warn(
+    clippy::complexity,
+    clippy::correctness,
+    clippy::perf,
+    clippy::style,
+    clippy::undocumented_unsafe_blocks,
+    rust_2018_idioms
+)]
+
 use asr::{
     future::next_tick,
+    game_engine::unity::il2cpp::{Module, Version},
+    time::Duration,
     timer::{self, TimerState},
-    watcher::Pair,
     watcher::Watcher,
-    Process,
+    Address, Process,
 };
-mod settings;
-use settings::Settings;
-use std::collections::HashSet;
 
 asr::async_main!(stable);
 
 async fn main() {
-    // TODO: Set up some general state and settings.
-    let mut start_watcher = Watcher::<u8>::new(); //
-    let mut loading_watcher = Watcher::<u8>::new(); //
-    let mut final_boss_watcher = Watcher::<u64>::new(); //
-    let mut enemy_0_hp_watcher = Watcher::<u32>::new();
-    let mut splits = HashSet::<String>::new();
+    // Settings
     let settings = Settings::register();
-    let mut current_enemy = 0;
-    loop {
-        let process = match asr::get_os().ok().unwrap().as_str() {
-            _ => Process::wait_attach("SeaOfStars.exe").await,
-        };
-        let (main_module_base, _main_module_size) =
-            process.wait_module_range("GameAssembly.dll").await;
 
-        asr::print_message("Captured gameassembly");
+    loop {
+        // Hook to the target process
+        let process = Process::wait_attach("SeaOfStars.exe").await;
 
         process
             .until_closes(async {
-                // TODO: Load some initial information from the process.
+                // Once the target process has been found and attached to, set up the watchers in their default state
+                let mut watchers = Watchers::default();
+
+                // Perform our memory scraping here
+                let unity = Module::wait_attach(&process, Version::V2020).await;
+                asr::print_limited::<128>(&"=> Found Unity module");
+
+                let assembly = unity.wait_get_default_image(&process).await;
+                asr::print_limited::<128>(&"=> Found Assembly-CSharp.exe");
+
+                let title_sequence_manager = assembly
+                    .wait_get_class(&process, &unity, "TitleSequenceManager")
+                    .await;
+                asr::print_limited::<128>(&"=> Found class: \"TitleSequenceManager\"");
+                let title_sequence_manager_parent = title_sequence_manager
+                    .wait_get_parent(&process, &unity)
+                    .await;
+                asr::print_limited::<128>(&"   => Found parent");
+                let mut title_sequence_manager_parent_instance: Option<u32> = None;
+                let mut title_sequence_manager_character_selection_screen: Option<u32> = None;
+                let mut title_sequence_manager_parent_static_table: Option<Address> = None;
+
+                let character_selection_screen = assembly
+                    .wait_get_class(&process, &unity, "CharacterSelectionScreen")
+                    .await;
+                asr::print_limited::<128>(&"=> Found class: \"CharacterSelectionScreen\"");
+                let mut character_selection_screen_character_selected: Option<u32> = None;
+
+                let combat_manager_class = assembly
+                    .wait_get_class(&process, &unity, "CombatManager")
+                    .await;
+                asr::print_limited::<128>(&"=> Found class: \"CombatManager\"");
+                let combat_manager_class_parent =
+                    combat_manager_class.wait_get_parent(&process, &unity).await;
+                asr::print_limited::<128>(&"   => Found parent");
+                let mut combat_manager_class_parent_instance: Option<u32> = None;
+                let mut combat_manager_class_parent_static_table: Option<Address> = None;
+
+                let level_manager_class = assembly
+                    .wait_get_class(&process, &unity, "LevelManager")
+                    .await;
+                asr::print_limited::<128>(&"=> Found class: \"LevelManager\"");
+                let level_manager_class_parent =
+                    level_manager_class.wait_get_parent(&process, &unity).await;
+                asr::print_limited::<128>(&"   => Found parent");
+                let mut level_manager_class_parent_instance: Option<u32> = None;
+                let mut level_manager_class_loading: Option<u32> = None;
+                let mut level_manager_class_parent_static_table: Option<Address> = None;
+
+                asr::print_limited::<128>(&"=> Autosplitter ready");
+
                 loop {
-                    let start_lookup = start_watcher.update(
-                        process
-                            .read_pointer_path64(
-                                main_module_base,
-                                &vec![0x2EAA630, 0xB8, 0x10, 0x70, 0x0 + 0xDA],
-                            )
-                            .ok(),
-                    );
-                    let start = match start_lookup {
-                        Some(start_value) => start_value,
-                        None => &Pair { old: 0, current: 0 },
-                    };
+                    // Looking for offsets
+                    if title_sequence_manager_parent_instance.is_none() {
+                        title_sequence_manager_parent_instance =
+                            title_sequence_manager_parent.get_field(&process, &unity, "instance");
+                    }
 
-                    let enemy_0_hp_lookup = enemy_0_hp_watcher.update(
-                        process
-                            .read_pointer_path64(
-                                main_module_base,
-                                &vec![0x327E360, 0x8E8, 0x10, 0x80, 0x40, 0x80, 0x6C],
-                            )
-                            .ok(),
-                    );
-                    let enemy_0_hp = match enemy_0_hp_lookup {
-                        Some(start_value) => start_value,
-                        None => &Pair {
-                            old: 9999,
-                            current: 9999,
-                        },
-                    };
+                    if title_sequence_manager_character_selection_screen.is_none() {
+                        title_sequence_manager_character_selection_screen = title_sequence_manager
+                            .get_field(&process, &unity, "characterSelectionScreen");
+                    }
 
-                    let final_boss_name_lookup: Option<&Pair<u64>> = final_boss_watcher.update(
+                    if title_sequence_manager_parent_static_table.is_none() {
+                        title_sequence_manager_parent_static_table =
+                            title_sequence_manager_parent.get_static_table(&process, &unity);
+                    }
+
+                    if character_selection_screen_character_selected.is_none() {
+                        character_selection_screen_character_selected = character_selection_screen
+                            .get_field(&process, &unity, "characterSelected");
+                    }
+
+                    if combat_manager_class_parent_instance.is_none() {
+                        combat_manager_class_parent_instance =
+                            combat_manager_class_parent.get_field(&process, &unity, "instance");
+                    }
+
+                    if combat_manager_class_parent_static_table.is_none() {
+                        combat_manager_class_parent_static_table =
+                            combat_manager_class_parent.get_static_table(&process, &unity);
+                    }
+
+                    if level_manager_class_parent_instance.is_none() {
+                        level_manager_class_parent_instance =
+                            level_manager_class_parent.get_field(&process, &unity, "instance");
+                    }
+
+                    if level_manager_class_loading.is_none() {
+                        level_manager_class_loading =
+                            level_manager_class.get_field(&process, &unity, "loadingLevel");
+                    }
+
+                    if level_manager_class_parent_static_table.is_none() {
+                        level_manager_class_parent_static_table =
+                            level_manager_class_parent.get_static_table(&process, &unity);
+                    }
+
+                    // Your update logic
+                    let start_autosplitter = if title_sequence_manager_parent_instance.is_some()
+                        && title_sequence_manager_parent_static_table.is_some()
+                        && title_sequence_manager_character_selection_screen.is_some()
+                        && character_selection_screen_character_selected.is_some()
+                    {
                         process
-                            .read_pointer_path64(
-                                main_module_base,
-                                &vec![
-                                    0x2EAAB30, 0xB8, 0x10, 0xF0, 0x118, 0x10, 0x20, 0x100, 0x18,
-                                    0x14,
+                            .read_pointer_path64::<u8>(
+                                title_sequence_manager_parent_static_table.unwrap_or_default(),
+                                &[
+                                    title_sequence_manager_parent_instance
+                                        .unwrap_or_default()
+                                        .into(),
+                                    title_sequence_manager_character_selection_screen
+                                        .unwrap_or_default()
+                                        .into(),
+                                    character_selection_screen_character_selected
+                                        .unwrap_or_default()
+                                        .into(),
                                 ],
                             )
-                            .ok(),
-                    );
-                    match final_boss_name_lookup {
-                        Some(final_boss_name) => {
-                            current_enemy = final_boss_name.current;
-                            final_boss_name
-                        }
-                        None => &Pair { old: 0, current: 0 },
+                            .unwrap_or_default()
+                            > 0
+                    } else {
+                        false
                     };
-                    
-                    let loading = loading_watcher
-                        .update(
-                            process
-                                .read_pointer_path64(
-                                    main_module_base,
-                                    &vec![0x2EAA510, 0xB8, 0x10, 0x70],
-                                )
-                                .ok(),
-                        )
-                        .unwrap();
+                    watchers
+                        .start_autosplitter
+                        .update_infallible(start_autosplitter);
 
-                    // Scenario Progress
+                    let loading = if level_manager_class_parent_instance.is_some()
+                        && level_manager_class_loading.is_some()
+                        && level_manager_class_parent_static_table.is_some()
+                    {
+                        process
+                            .read_pointer_path64::<u8>(
+                                level_manager_class_parent_static_table.unwrap_or_default(),
+                                &[
+                                    level_manager_class_parent_instance
+                                        .unwrap_or_default()
+                                        .into(),
+                                    level_manager_class_loading.unwrap_or_default().into(),
+                                ],
+                            )
+                            .unwrap_or_default()
+                            > 0
+                    } else {
+                        true
+                    };
+                    watchers.loading.update_infallible(loading);
 
-                    match timer::state() {
-                        TimerState::NotRunning => {
-                            if settings.start && start.old == 0 && start.current == 1 {
-                                // asr::print_message("Clearing Splits and Starting");
-                                splits = HashSet::<String>::new();
-                                timer::start();
+                    let chromatic_apparition = if level_manager_class_parent_instance.is_some()
+                        && level_manager_class_loading.is_some()
+                        && level_manager_class_parent_static_table.is_some()
+                    {
+                        process
+                            .read_pointer_path64::<u8>(
+                                combat_manager_class_parent_static_table.unwrap_or_default(),
+                                &[
+                                    level_manager_class_parent_instance
+                                        .unwrap_or_default()
+                                        .into(),
+                                    level_manager_class_loading.unwrap_or_default().into(),
+                                ],
+                            )
+                            .unwrap_or_default()
+                            > 0
+                    } else {
+                        true
+                    };
+                    watchers.loading.update_infallible(loading);
+                    // The update logic ends here
+
+                    // Splitting logic
+                    let timer_state = timer::state();
+                    if timer_state == TimerState::Running || timer_state == TimerState::Paused {
+                        if let Some(is_loading) = is_loading(&watchers, &settings) {
+                            if is_loading {
+                                timer::pause_game_time()
+                            } else {
+                                timer::resume_game_time()
                             }
                         }
-                        TimerState::Running => {
-                            // CHAPTER SPLITS
 
-                            if settings.chromatic_apparition
-                                && current_enemy == 14918388517371959
-                                && enemy_0_hp.old > 0
-                                && enemy_0_hp.current < 1
-                            {
-                                // asr::print_message("SPLIT FINAL BOSS");
-                                split(&mut splits, "final_split")
-                            }
-
-                            if settings.load_removal {
-                                // load/save removal
-
-                                if loading.old == 1 && loading.current == 0 {
-                                    // asr::print_message("resuming game time");
-                                    timer::resume_game_time()
-                                }
-
-                                if loading.old == 0 && loading.current == 1 {
-                                    // asr::print_message("pausing game time");
-                                    timer::pause_game_time()
-                                }
-                            }
+                        if let Some(game_time) = game_time(&watchers, &settings) {
+                            timer::set_game_time(game_time)
                         }
-                        _ => {}
+
+                        if reset(&watchers, &settings) {
+                            timer::reset()
+                        } else if split(&watchers, &settings) {
+                            timer::split()
+                        }
                     }
-                    // TODO: Do something on every tick.
+
+                    if timer::state() == TimerState::NotRunning && start(&watchers, &settings) {
+                        timer::start();
+                        timer::pause_game_time();
+
+                        if let Some(is_loading) = is_loading(&watchers, &settings) {
+                            if is_loading {
+                                timer::pause_game_time()
+                            } else {
+                                timer::resume_game_time()
+                            }
+                        }
+                    }
+
                     next_tick().await;
                 }
             })
@@ -138,10 +241,51 @@ async fn main() {
     }
 }
 
-pub fn split(splits: &mut HashSet<String>, key: &str) {
-    if !splits.contains(key) {
-        splits.insert(key.to_string());
-        asr::print_message(&key.to_string());
-        timer::split()
+fn is_loading(watchers: &Watchers, settings: &Settings) -> Option<bool> {
+    if !settings.load_removal {
+        Some(false)
+    } else {
+        Some(watchers.loading.pair?.current)
     }
+}
+
+fn game_time(watchers: &Watchers, settings: &Settings) -> Option<Duration> {
+    None
+}
+
+fn reset(watchers: &Watchers, settings: &Settings) -> bool {
+    false
+}
+
+fn start(watchers: &Watchers, settings: &Settings) -> bool {
+    if !settings.start_autosplitter {
+        return false;
+    }
+    let Some(start_autosplitter) = &watchers.start_autosplitter.pair else { return false };
+    !start_autosplitter.old && start_autosplitter.current
+}
+
+fn split(_watchers: &Watchers, _settings: &Settings) -> bool {
+    false
+}
+
+#[derive(asr::user_settings::Settings)]
+struct Settings {
+    #[default = true]
+    /// Load Removal
+    load_removal: bool,
+    #[default = true]
+    /// Automatic Start on character select
+    start_autosplitter: bool,
+    #[default = true]
+    /// Split on defeating Chromatic Apparition
+    chromatic_apparition: bool,
+}
+
+#[derive(Default)]
+struct Watchers {
+    start_autosplitter: Watcher<bool>,
+    loading: Watcher<bool>,
+    chromatic_apparition: Watcher<u64>,
+    enemy_0_hp: Watcher<u32>,
 }
